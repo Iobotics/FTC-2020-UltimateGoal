2fd3514d91ccfb6b2046dd9ee71b298cc873d48f
MegaRequaza <ats2501@iolani.org>
2020-10-08 16:54:20 -1000

    Added Servo functionality.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -4,11 +4,16 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 public class Bot {
     private LinearOpMode opMode;
     private DcMotor leftMotor;
     private DcMotor rightMotor;
+    private Servo testServo1 = null;
+    private Servo testServo2 = null;
 
     public Bot(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -20,10 +25,19 @@ public class Bot {
         rightMotor = map.get(DcMotor.class, "right-motor");
 
         rightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
-    }
+        testServo1 = map.get(Servo.class, "testServo1");
+        testServo2 = map.get(Servo.class,"testServo2");
+   }
 
     public void setDriveTrain(double left, double right) {
         leftMotor.setPower(left);
         rightMotor.setPower(right);
     }
+    public void setTestServo1(double position) {
+        testServo1.setPosition(position);
+    }
+    public void setTestServo2(double position) {
+        testServo2.setPosition(position);
+    }
+
 }
