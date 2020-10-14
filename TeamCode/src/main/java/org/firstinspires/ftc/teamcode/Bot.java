20eacdec747c1703dc08996a230d0defc40f6800
3xtr4ct <[cti2501@iolani.org]>
2020-10-13 16:41:18 -1000

    Code for Elvis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Bot.java
@@ -10,14 +10,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
 public class Bot {
     private LinearOpMode opMode;
-    private DcMotor leftMotorFront;
-    private DcMotor rightMotorFront;
-    private DcMotor leftMotorBack;
-    private DcMotor rightMotorBack;
-    private DcMotor intakeRight;
-    private DcMotor intakeLeft;
-    private DcMotor lifterLeft;
-    private DcMotor lifterRight;
+    private DcMotor motor1;
+    private DcMotor motor2;
 
     public Bot(LinearOpMode opMode) {
         this.opMode = opMode;
@@ -25,38 +19,13 @@ public class Bot {
     }
 
     public void init(HardwareMap map) {
-        leftMotorFront = map.get(DcMotor.class, "left-motor-front");
-        rightMotorFront = map.get(DcMotor.class, "right-motor-front");
-        leftMotorBack = map.get(DcMotor.class, "left-motor-back");
-        rightMotorBack = map.get(DcMotor.class, "right-motor-back");
-        intakeLeft = map.get(DcMotor.class, "intake-left");
-        intakeRight = map.get(DcMotor.class, "intake-right");
-        lifterLeft = map.get(DcMotor.class, "lifter-left");
-        lifterRight = map.get(DcMotor.class, "lifterRight");
-
-
-        rightMotorFront.setDirection(DcMotorSimple.Direction.REVERSE);
-        rightMotorBack.setDirection(DcMotorSimple.Direction.REVERSE);
-        lifterRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        lifterLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        intakeRight.setDirection(DcMotorSimple.Direction.REVERSE);
-        intakeLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-
+        motor1 = map.get(DcMotor.class, "motor1");
+        motor2 = map.get(DcMotor.class, "motor2");
    }
 
     public void setDriveTrain(double left, double right) {
-        leftMotorFront.setPower(left);
-        rightMotorFront.setPower(right);
-        leftMotorBack.setPower(left);
-        rightMotorBack.setPower(right);
-    }
-    public void setIntake(double power) {
-        intakeRight.setPower(power);
-        intakeLeft.setPower(power);
-    }
-    public void setLifter(double power) {
-        lifterLeft.setPower(power);
-        lifterRight.setPower(power);
+        motor1.setPower(left);
+        motor2.setPower(right);
     }
 
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -15,23 +15,6 @@ public class TeleOp extends LinearOpMode {
 
         while(opModeIsActive()) {
             bot.setDriveTrain(-gamepad1.left_stick_y, -gamepad1.right_stick_y);
-
-            if(gamepad1.b) {
-                bot.setIntake(1);
-
-            }else if(gamepad1.y) {
-                bot.setIntake(-1);
-            }else {
-                bot.setIntake(0);
-            }
-            if(gamepad1.x) {
-                bot.setLifter(1);
-
-            }
-            if(gamepad1.a) {
-                bot.setLifter(0);
-            }
-
         }
     }
 }
