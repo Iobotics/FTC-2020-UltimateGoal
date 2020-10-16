0ecbb19ece142b24e74f46ff0e7ce044b02dd0c0
3xtr4ct <[cti2501@iolani.org]>
2020-10-15 16:14:36 -1000

    code upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,21 +1,4 @@
-
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-@Autonomous(name = "Test Auto", group = "auto")
-public class Auto extends LinearOpMode {
-    @Override
-    public void runOpMode() throws InterruptedException {
-        Bot bot = new Bot(this);
-
-        waitForStart();
-
-        bot.setDriveTrain(1, 1);
-        sleep(1000);
-        bot.setDriveTrain(1, -1);
-        sleep(500);
-        bot.setDriveTrain(0, 0);
-    }
+public class Auto {
 }
